243e4a6ea01fa01108b7630116e90197cff479da
Ignacio <ignaciojaviermedici9@gmail.com>
2023-05-25 20:56:12 -0300

    problema 5

--- funciones_parcial.py
+++ funciones_parcial.py
@@ -1,6 +1,7 @@
 import json 
 import re
 import os
+import random
 #Funcion de lectura de archivos json
 
 def leer_json(direccion:str)->list:
@@ -12,4 +13,30 @@ def leer_json(direccion:str)->list:
             contenido = archivo.read()
             lista = json.loads(contenido)
             return lista
-        
+
+
+def quick_sort(lista:list, orden:str)->list:
+    lista_copia = lista.copy()
+    lista_menores=[]
+    lista_mayores=[]
+    lista_ordenada=[]
+    if len(lista) < 2:
+        return lista
+    else:
+        pivote = lista_copia[0]
+        for item in lista_copia[1:]:
+            if orden == "ascendente" or orden == "asc":
+                if (item < pivote):
+                    lista_menores.append(item)
+                else:
+                    lista_mayores.append(item)
+            elif orden == "descendente" or orden == "desc":
+                if (item < pivote):
+                    lista_mayores.append(item)
+                else:
+                    lista_menores.append(item)
+    lista_menores = quick_sort(lista_menores, orden)
+    lista_menores.append(pivote)
+    lista_mayores = quick_sort(lista_mayores, orden)
+    lista_ordenada = lista_menores + lista_mayores
+    return lista_ordenada
--- pp_lab1.py
+++ pp_lab1.py
@@ -1,14 +1,14 @@
 from funciones_parcial import *
 os.system("clear")
 lista_dt = leer_json("dt.json")
-
+#1
 def listar_jugadores(lista_jugaodres:list):
     if len(lista_jugaodres) > 0:
         for jugador in lista_jugaodres["jugadores"]:
             print("Nombre: {0} Posicion: {1}".format(jugador["nombre"], jugador["posicion"]))
     else:
         print("No hay jugadores en la lista")
-
+#2
 def estadisticas_jugador(lista_jugadores:list,indice:int)-> list:
     lista_estadisticas = []
     if indice < len(lista_jugadores["jugadores"]):
@@ -31,19 +31,33 @@ def estadisticas_jugador(lista_jugadores:list,indice:int)-> list:
     else:
         print("El índice del jugador está fuera de rango")
     return lista_estadisticas
-
+#3
 def exportar_estadisticas_csv(estadisticas:list,jugador:dict):
+    archivo_csv = "estadisticas_jugadores/estadisticas_{0}.csv".format(jugador["nombre"]).replace(" ","_").lower()
     if len(estadisticas) > 0:
-        with open("estadisticas.csv","w") as archivo:
+        with open(archivo_csv,"w") as archivo:
             archivo.write("nombre_jugador,posicion_posicion,temporadas,puntos_totales,promedio_puntos_por_partido,rebotes_totales,promedio_rebotes_por_partido,asistencias_totales,robos_totales,bloqueos_totales,porcentaje_tiros_de_campo,porcentaje_tiros_libres,porcentaje_tiros_triples\n")
             for estadistica in estadisticas:
                 archivo.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}\n".format(jugador["nombre"],jugador["posicion"],estadistica["temporadas"],estadistica["puntos_totales"],estadistica["promedio_puntos_por_partido"],estadistica["rebotes_totales"],estadistica["promedio_rebotes_por_partido"],estadistica["asistencias_totales"],estadistica["robos_totales"],estadistica["bloqueos_totales"],estadistica["porcentaje_tiros_de_campo"],estadistica["porcentaje_tiros_libres"],estadistica["porcentaje_tiros_triples"]))
+#4
+def logros_jugadores(jugador:dict):
+    for logros in jugador["logros"]:
+        print(logros)
 
-
+#5 
+def promedio_puntos_por_partido(lista_jugadores:list)-> float:
+    acumulador_promedios = 0
+    contador_promedios = 0
+    for jugador in lista_jugadores["jugadores"]:
+        acumulador_promedios += jugador["estadisticas"]["promedio_puntos_por_partido"]
+        contador_promedios += 1
+    promedio = acumulador_promedios / contador_promedios
+    return promedio
 
 # listar_jugadores(lista_dt)
-indice = int(input("Ingrese el indice del jugador: "))
-resultado = estadisticas_jugador(lista_dt,indice)
-exportar_estadisticas_csv(resultado,lista_dt["jugadores"][indice])
-
-
+# indice = int(input("Ingrese el indice del jugador: "))
+# resultado = estadisticas_jugador(lista_dt,indice)
+# exportar_estadisticas_csv(resultado,lista_dt["jugadores"][indice])
+# logros_jugadores(lista_dt["jugadores"][indice])
+resultado = promedio_puntos_por_partido(lista_dt)
+print("El promedio de los puntos por partido es: {0:.2f}".format(resultado))
